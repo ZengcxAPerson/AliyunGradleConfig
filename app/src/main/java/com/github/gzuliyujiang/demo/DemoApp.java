f9c289b2c0d60e85f1a5b68532261c80d6aa17b0
liyujiang <1032694760@qq.com>
2020-06-22 13:56:20 +0800

    fix: compile error

--- app/src/main/java/com/github/gzuliyujiang/demo/DemoApp.java
+++ app/src/main/java/com/github/gzuliyujiang/demo/DemoApp.java
@@ -1,14 +1,6 @@
 /*
- * Copyright (c) 2019-2020 gzu-liyujiang <1032694760@qq.com>
+ * Copyright (c) 2016-present, 贵州纳雍穿青人李裕江<1032694760@qq.com>, All Right Reserved.
  *
- * AliyunGradleConfig is licensed under the Mulan PSL v1.
- * You can use this software according to the terms and conditions of the Mulan PSL v1.
- * You may obtain a copy of Mulan PSL v1 at:
- *     http://license.coscl.org.cn/MulanPSL
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
- * PURPOSE.
- * See the Mulan PSL v1 for more details.
  */
 package com.github.gzuliyujiang.demo;
 
@@ -24,7 +16,7 @@ public class DemoApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Logger.ENABLE = BuildConfig.DEBUG;
+        Logger.useDefaultPrinter();
     }
 
 }
---
+++ library/src/main/java/com/github/gzuliyujiang/library/Test.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2016-present, 贵州纳雍穿青人李裕江<1032694760@qq.com>, All Right Reserved.
+ *
+ */
+
+package com.github.gzuliyujiang.library;
+
+/**
+ * Created by liyujiang on 2020/6/22.
+ */
+public class Test {
+}
