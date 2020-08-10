fec06882497845520c8d9ff1d5862789da44a8b2
liyujiang <1032694760@qq.com>
2020-08-10 11:38:27 +0800

    build: 忽略Lambda表达式的lint

--- app/src/main/java/com/github/gzuliyujiang/demo/MainActivity.java
+++ app/src/main/java/com/github/gzuliyujiang/demo/MainActivity.java
@@ -15,9 +15,9 @@ package com.github.gzuliyujiang.demo;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
