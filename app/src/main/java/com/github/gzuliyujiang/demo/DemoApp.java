2ad16fab693f7019aaab62f229b60c924272a163
liyujiang <1032694760@qq.com>
2021-05-10 18:57:18 +0800

    remove Logger

--- app/src/main/java/com/github/gzuliyujiang/demo/DemoApp.java
+++ app/src/main/java/com/github/gzuliyujiang/demo/DemoApp.java
@@ -15,21 +15,14 @@ package com.github.gzuliyujiang.demo;
 
 import android.app.Application;
 
-import com.github.gzuliyujiang.logger.Logger;
-
 /**
  * Created by liyujiang on 2020/5/20.
  */
 public class DemoApp extends Application {
 
-    static {
-        Logger.enableConsolePrinter();
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
-        Logger.print("APP created");
     }
 
 }
